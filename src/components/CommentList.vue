64ccc65918c5802caa94f247f066557836ad6170
tcm <tylermckay10@gmail.com>
2022-04-07 10:13:48 -0300

    added comment list component

---
+++ src/components/CommentList.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="box m-5" v-if="comments.length > 0">
+        <div class="block content" v-for="comment in comments" :key="comment.comment_id">
+            <h5>{{ comment.user?.username }}</h5>
+            <p>{{ comment.content }}</p>
+        </div>
+    </div>
+    <div class="box content m-5" v-else>
+        <i>There are no comments yet...</i>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { Comment } from "../api/types"
+
+defineProps<{
+    comments: Comment[]
+}>()
+
+</script>
