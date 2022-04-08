7110309fbed7ad3e89558a232dbcfcb06b7cdbf2
tcm <tylermckay10@gmail.com>
2022-04-08 10:48:53 -0300

    search bar component

---
+++ src/components/Search.vue
@@ -0,0 +1,18 @@
+<template>
+    <form class="fields is-grouped">
+        <input type="text" placeholder="search..." v-model="searchText" />
+        <button type="submit" @click="$emit('search', searchText)">Search</button>
+    </form>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const searchText = ref("");
+
+defineEmits([
+    "search"
+])
+
+
+</script>
