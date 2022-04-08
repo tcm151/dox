d466850dd9f0c3aba97eca201c5bf057e0e39ca3
tcm <tylermckay10@gmail.com>
2022-04-08 13:09:33 -0300

    fixed some funky stuff

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,8 @@ import Main from "./Main.vue"
 import { createApp } from "vue"
 import { store } from "./services/store"
 import { router } from "./services/router"
+// import { app } from "./api/server"
 
 createApp(Main).use(router).use(store).mount("#app")
+
+// app.emit("start")
