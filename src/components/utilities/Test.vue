add7837a9abe2e14b52ce90850bd2ecc43405880
tcm <tylermckay10@gmail.com>
2022-10-06 03:21:51 -0300

    fixed build issues

---
+++ src/components/utilities/Test.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const email = ref('')
+</script>
+
+<template>
+    <input v-model="email" />
+</template>
--- src/views/Dev.vue
+++ src/views/Dev.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import TextEditor from '../components/utilities/TextEditor.vue';
+
+const text = ref("");
 
 </script>
 
 <template>
     <div class="dev box">
-        <TextEditor />
+        <TextEditor :text="text" />
     </div>
 </template>
 
