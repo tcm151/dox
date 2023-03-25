6097c122611d98870b0ad356dc663ee332120459
tcm <tylermckay10@gmail.com>
2023-03-25 13:39:53 -0300

    fixed final voting issues

--- services/voting.ts
+++ services/voting.ts
@@ -12,21 +12,17 @@ export const useVoting = () => {
             return
         }
 
+        votes.misleading = votes.misleading.filter((id) => id !== session.user!.id)
+        votes.negative = votes.negative.filter((id) => id !== session.user!.id)
+
         if (!votes.positive.includes(session.user!.id)) {
             votes.positive.push(session.user!.id)
+            await session.useApi(`/api/vote/${id}/positive`)
         }
         else {
             votes.positive = votes.positive.filter((id) => id !== session.user!.id)
+            await session.useApi(`/api/vote/${id}/reset`)
         }
-
-        if (votes.misleading.includes(session.user!.id)) {
-            votes.misleading = votes.misleading.filter((id) => id !== session.user!.id)
-        }
-        if (votes.negative.includes(session.user!.id)) {
-            votes.negative = votes.negative.filter((id) => id !== session.user!.id)
-        }
-
-        await session.useApi(`/api/vote/${id}/positive`)
     }
 
     async function misleading(id: string, votes: Votes) {
@@ -35,44 +31,37 @@ export const useVoting = () => {
             return
         }
 
+        votes.positive = votes.positive.filter((id) => id !== session.user!.id)
+        votes.negative = votes.negative.filter((id) => id !== session.user!.id)
+        
         if (!votes.misleading.includes(session.user!.id)) {
             votes.misleading.push(session.user!.id)
+            await session.useApi(`/api/vote/${id}/misleading`)
         }
         else {
             votes.misleading = votes.misleading.filter((id) => id !== session.user!.id)
+            await session.useApi(`/api/vote/${id}/reset`)
         }
-
-        if (votes.positive.includes(session.user!.id)) {
-            votes.positive = votes.positive.filter((id) => id !== session.user!.id)
-        }
-        if (votes.negative.includes(session.user!.id)) {
-            votes.negative = votes.negative.filter((id) => id !== session.user!.id)
-        }
-
-        await session.useApi(`/api/vote/${id}/misleading`)
     }
-
+    
     async function negative(id: string, votes: Votes) {
         if (!session.isAuthenticated) {
             hints.addError("You must be logged in to interact with others.")
             return
         }
-
+        
+        votes.positive = votes.positive.filter((id) => id !== session.user!.id)
+        votes.misleading = votes.misleading.filter((id) => id !== session.user!.id)
+        
         if (!votes.negative.includes(session.user!.id)) {
             votes.negative.push(session.user!.id)
+            await session.useApi(`/api/vote/${id}/negative`)
         }
         else {
             votes.negative = votes.negative.filter((id) => id !== session.user!.id)
+            await session.useApi(`/api/vote/${id}/reset`)
         }
-
-        if (votes.positive.includes(session.user!.id)) {
-            votes.positive = votes.positive.filter((id) => id !== session.user!.id)
-        }
-        if (votes.misleading.includes(session.user!.id)) {
-            votes.misleading = votes.misleading.filter((id) => id !== session.user!.id)
-        }
-
-        await session.useApi(`/api/vote/${id}/negative`)
+        
     }
 
     return { positive, misleading, negative }
