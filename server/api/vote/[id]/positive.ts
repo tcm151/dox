f9b0edaaad14dc675e636d93cc910179372a848d
tcm <tylermckay10@gmail.com>
2023-03-25 21:24:11 -0300

    fixed votes

--- server/api/vote/[id]/positive.ts
+++ server/api/vote/[id]/positive.ts
@@ -1,6 +1,5 @@
 import { queryOne, authenticateRequest } from "../../../database"
 
-// FIXME can be abused by authenticated user
 export default defineEventHandler(async (event) => {
     const { id } = event.context.params!;
     const auth = await authenticateRequest(event);
