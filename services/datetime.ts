bc185f69d8251cd91cbdd2c82571a00e1240d1d2
tcm <tylermckay10@gmail.com>
2023-04-05 22:31:50 -0300

    use luxon

--- services/datetime.ts
+++ services/datetime.ts
@@ -1,28 +1,28 @@
+import { DateTime } from "luxon";
+
 export function formatDate(dateString: string): string {
-    const ms = Date.parse(new Date().toISOString()) - Date.parse(dateString);
-    const minutes = ms / 1000 / 60
-    const hours = minutes / 60;
-    const days = hours / 24;
-    const weeks = days / 7;
-    const months = weeks / 4;
-    const years = days / 365;
-    if (days >= 365) {
-        return `${years.toFixed(0)}yr ago`;
+    
+    const duration = DateTime.now().diff(DateTime.fromISO(dateString), [
+        'minutes', 'hours', 'days', 'weeks', 'months', 'years'
+    ])
+
+    if (duration.years >= 1) {
+        return `${duration.years.toFixed(0)}yr ago`;
     }
-    if (weeks >= 4) {
-        return `${months.toFixed(0)}mo ago`;
+    if (duration.months >= 1) {
+        return `${duration.months.toFixed(0)}mo ago`;
     }
-    if (days >= 7) {
-        return `${weeks.toFixed(0)}wk ago`;
+    if (duration.weeks >= 1) {
+        return `${duration.weeks.toFixed(0)}wk ago`;
     }
-    if (hours >= 24) {
-        return `${days.toFixed(0)}d ago`;
+    if (duration.days >= 24) {
+        return `${duration.days.toFixed(0)}d ago`;
     }
-    if (hours >= 1) {
-        return `${hours.toFixed(0)}h ago`;
+    if (duration.hours >= 1) {
+        return `${duration.hours.toFixed(0)}h ago`;
     }
-    if (minutes >= 1) {
-        return `${minutes.toFixed(0)}m ago`;
+    if (duration.minutes >= 1) {
+        return `${duration.minutes.toFixed(0)}m ago`;
     }
     else {
         return '< 1m ago'
