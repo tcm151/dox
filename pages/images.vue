96a2d4111ffb0ae4afd7de8c2066b0575560c733
tcm <tylermckay10@gmail.com>
2023-04-12 22:07:39 -0300

    test uploading base64 images

---
+++ pages/images.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+let base64 = ref<string>("")
+
+async function readFiles(event: Event) {
+    const fileReader = new FileReader()
+    const files = (event.target as HTMLInputElement).files
+    fileReader.readAsDataURL(files![0])
+    
+    fileReader.onloadend = () => {
+        base64.value = fileReader.result as string
+    }
+}
+</script>
+
+<template>
+    <input type="file" @change="readFiles" />
+    <img :src="base64">
+</template>
+
+<style scoped lang="scss">
+img {
+    height: 256px;
+}
+</style>
