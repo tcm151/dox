ddab48b40a1dae37c447389407ca1a13a12f17c0
TCM <tylermckay10@gmail.com>
2023-04-19 19:33:44 -0300

    sanitize html

--- utils/markdown.ts
+++ utils/markdown.ts
@@ -1,12 +1,18 @@
 import { marked } from 'marked'
-import { decode } from 'html-entities';
-import hljs from "highlight.js/lib/common";
+import { decode } from 'html-entities'
+import sanitizeHtml, { IOptions } from "sanitize-html"
+import hljs from "highlight.js/lib/common"
 
 hljs.configure({ ignoreUnescapedHTML: true })
 
+const sanitizationOptions: IOptions = {
+    allowedTags: sanitizeHtml.defaults.allowedTags.concat([ 'img' ])
+}
+
 export function renderMarkdown(text: string | undefined) {
     const html = marked.parse(text ?? "")
-    const decoded = decode(html)
+    const sanitized = sanitizeHtml(html, sanitizationOptions)
+    const decoded = decode(sanitized)
     const colorized = colorize(decoded)
     return colorized
 }
