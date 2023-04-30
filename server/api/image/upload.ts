01ac2540c2cd7c76c5a225667cf492a8600981df
tcm <tylermckay10@gmail.com>
2023-04-30 20:24:39 -0300

    working on accepting multiple file types, including .gifs

--- server/api/image/upload.ts
+++ server/api/image/upload.ts
@@ -1,5 +1,6 @@
 import fs from "node:fs"
 import sharp from "sharp"
+import { MultiPartData } from "h3"
 import { authenticateRequest, queryOne } from "~/server/database"
 import { Image } from "~/types"
 
@@ -10,22 +11,30 @@ export default defineEventHandler(async (event) => {
     const baseUrl = getHeader(event, 'origin')
     const image = await queryOne<Image>([`
         CREATE image SET
+        time = time::now()
         url = <future> { string::concat("${baseUrl}/image/", string::split(id, ":")[1]) },
         user = ${auth.id},
-        time = time::now()
     `])
-    const png = await sharp(data![0].data).png().toBuffer()
+
+    const buffer = await processImage(data![0])
     switch (process.env.NODE_ENV) {
         case "development":
-            fs.writeFileSync(`./images/${image.id.split(':')[1]}.png`, png, {
+            fs.writeFileSync(`./images/${image.id.split(':')[1]}.png`, buffer, {
                 flag: "w+"
             })
             break
         case "production":
-            fs.writeFileSync(`./.output/images/${image.id.split(':')[1]}.png`, png, {
+            fs.writeFileSync(`./.output/images/${image.id.split(':')[1]}.png`, buffer, {
                 flag: "w+"
             })
             break
     }
     return image
 })
+
+async function processImage(image: MultiPartData) {
+    switch (image.type) {
+        default:
+            return await sharp(image.data).png().toBuffer()
+    }
+}
