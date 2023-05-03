a46d04130735f4546139af64537ba584730dbe0a
tcm <tylermckay10@gmail.com>
2023-05-02 23:16:33 -0300

    organize significantly

--- server/database.ts
+++ server/database.ts
@@ -1,66 +1,61 @@
 import Surreal from 'surrealdb.js';
-import { User } from '~/types';
 
-export const useDatabase = async () => {
+const db = await (async () => {
+    const { surreal } = useRuntimeConfig();
+    if (surreal.url == "" || !surreal.url.includes("/rpc")) {
+        throw createError({
+            statusCode: 500,
+            message: `Database URL was [${surreal.url ?? "empty"}]. Check environment variables.`
+        })
+    }
+    const db = new Surreal(surreal.url);
     try {
-        const { surreal } = useRuntimeConfig();
-        // INFO url needs to be https://link.to.db/rpc
-        const db = new Surreal(surreal.url);
         await db.signin({ user: surreal.username, pass: surreal.password });
         await db.use(surreal.namespace, surreal.database);
         console.log(`Connected to ${surreal.namespace}:${surreal.database}`)
-        return db
     }
     catch (ex: any) {
+        console.log("Unable to connect to ${surreal.namespace}:${surreal.database}...")
         console.log(ex.message)
     }
-}
-const db = await useDatabase();
+    return db
+})()
 
-export const authenticateRequest = async (event: any) => {
-    try {
-        const { surreal } = useRuntimeConfig();
-        const db = new Surreal(surreal.url);
-        const token = getHeader(event, 'Authorization');
-        await db.authenticate(token ?? "");
-        let response = await db.query<any>("SELECT * FROM user WHERE id = $auth.id")
-        return (response[0].result[0] as User);
-    }
-    catch (ex) {
-        throw createError({ statusCode: 401, message: "Failed to authenticate request." })
-    }
-}
 
-interface QueryParameters {
+
+interface Parameters {
     [key: string]: any
 }
 
-interface DatabaseResponse<T> {
+export interface DatabaseResponse<T> {
     status: string
     details?: string
     time: string
     result: T[]
 }
 
-async function handleQuery<T>(sql: string[], parameters?: QueryParameters) {
-    await db?.wait()
-    const response = await db?.query(sql.join("\n"), parameters ?? {}) as DatabaseResponse<T>[]
+async function handleQuery<T>(sql: string[], parameters?: Parameters) {
+    await db.wait()
+    const response = await db.query(sql.join("\n"), parameters ?? {}) as DatabaseResponse<T>[]
     if (response[0].status == 'ERR') {
-        throw createError({ statusCode: 500, message: response[0].details })
+        throw createError({
+            statusCode: 500,
+            message: response[0].details
+        })
     }
     return response
 }
 
-export async function queryOne<T>(sql: string[], parameters?: QueryParameters): Promise<T> {
+export async function queryOne<T>(sql: string[], parameters?: Parameters): Promise<T> {
     let responses = await handleQuery<T>(sql, parameters)
     return responses[0].result[0]
 }
 
-export async function queryAll<T>(sql: string[], parameters?: QueryParameters): Promise<T[]> {
+export async function queryAll<T>(sql: string[], parameters?: Parameters): Promise<T[]> {
     let response = await handleQuery<T>(sql, parameters) 
     return response[0].result
 }
 
-export async function multiQuery(sql: string[], parameters?: QueryParameters): Promise<void> {
+export async function multiQuery(sql: string[], parameters?: Parameters): Promise<void> {
     await handleQuery(sql, parameters)
 }
