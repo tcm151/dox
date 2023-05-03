1cbe36426e99cbc33df74c807b41bdd0da43b493
tcm <tylermckay10@gmail.com>
2023-05-02 22:44:54 -0300

    throw error if user doesn't exist during authentication

--- server/api/profile/index.ts
+++ server/api/profile/index.ts
@@ -3,6 +3,14 @@ import { queryOne, authenticateRequest } from "~/server/database";
 
 export default defineEventHandler(async (event) => {
     const auth = await authenticateRequest(event);
+    
+    if (!auth) {
+        throw createError({
+            statusCode: 400,
+            statusMessage: "User does not exist."
+        })
+    }
+    
     return await queryOne<User>([`
         SELECT id, name, email, dateCreated, followers, following, topics, admin
         FROM user
