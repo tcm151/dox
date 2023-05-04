63fcb6b4e6ccb075e9ab1f8c43d5e3a9eebe6f69
tcm <tylermckay10@gmail.com>
2023-05-03 23:34:06 -0300

    move option down

--- components/system/Settings.client.vue
+++ components/system/Settings.client.vue
@@ -18,10 +18,6 @@ const settings = useUserSettings()
     <!-- TODO add two-factor authentication -->
     <Window :visible="visible" title="Settings" @close="emit('close')">
         <div class="form">
-            <div class="field row-fit">
-                <label>hint duration (ms)</label>
-                <input type="number" size="4" step="250" v-model="settings.state.hintDuration">
-            </div>
             <div class="row-fit g-2">
                 <input type="checkbox" v-model="settings.state.hoverAnimations">
                 <label>enable animations</label>
@@ -29,6 +25,10 @@ const settings = useUserSettings()
             <div class="row-fit g-2">
                 <input type="checkbox" v-model="settings.state.showPreviewByDefault">
                 <label>show preview by default</label>
+            </div>
+            <div class="field row-fit">
+                <label>hint duration (ms)</label>
+                <input type="number" size="4" step="250" v-model="settings.state.hintDuration">
             </div>
         </div>
     </Window>
