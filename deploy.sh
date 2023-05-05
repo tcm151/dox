80f581cd2ec322a680c2e3c21386da6ec1ed931a
tcm <tylermckay10@gmail.com>
2023-05-05 01:09:37 +0000

    include deploy script in repo

---
+++ deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# exit if there are any failures
+set -e
+
+{
+	# only run if --build is included
+	if [[ "$*" == *--build ]]; then	
+   		# pull latest changes
+   		git pull
+   		# update packages and build
+   		yarn install && yarn build
+   		# kill running server
+   		kill $(lsof -t -i :8001) 2>/dev/null || true
+   		# delete production folder if it exists
+   		if [ -d ".production" ]; then
+   			rm -r .production
+   			echo "Deleted production folder..."
+   		fi
+   		# copy build to production
+   		cp -r .output .production
+   		echo "Replaced production with build output"
+   		# symlink image uploads to production
+   		ln -s ~/dox/images ~/dox/.production/images
+   		echo "Linked image upload directory to production"
+	else
+    	# kill running server
+    	kill $(lsof -t -i :8001) 2>/dev/null || true
+	fi
+} | tee build.log
+
+# start server in the background and direct output to log
+exec nohup yarn start:production >server.log 2>error.log &
+
+echo "Server deployed successfully..."
+exit 0
