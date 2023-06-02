5b3f0fb97cb976c16e7b7027717a5c370812e434
tcm <tylermckay10@gmail.com>
2023-06-02 01:38:10 -0300

    codeblock server component

---
+++ components/text/Codeblock.server.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const props = defineProps<{
+    code?: string
+    language?: string
+    wrap?: boolean
+}>()
+
+const markdown = computed(() => {
+    return [
+        `\`\`\`${props.language ?? ""}`,
+        `${props.code}`,
+        `\`\`\``,
+    ].join("\n")
+})
+</script>
+
+<template>
+    <div class="content" :class="{ wrap: wrap }" v-html="renderMarkdown(markdown)"></div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
