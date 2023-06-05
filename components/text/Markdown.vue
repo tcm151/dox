c0793d1ddac405fd1fa8a7ba6616ecca742b5668
tcm <tylermckay10@gmail.com>
2023-06-05 14:35:49 -0300

    add todo

--- components/text/Markdown.vue
+++ components/text/Markdown.vue
@@ -3,5 +3,7 @@ defineProps<{ content?: string }>()
 </script>
 
 <template>
+    <!-- TODO try to find alternative to passing props -->
+    <!-- issue when content is very large, receive error request-uri it too large -->
     <div v-html="renderMarkdown(content)"></div>
 </template>
