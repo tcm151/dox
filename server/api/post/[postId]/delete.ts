85eebc40732065f69603c3e39c9fa8c977eeaa54
tcm <tylermckay10@gmail.com>
2023-07-11 15:38:23 -0300

    refund tokens when deleting posts

--- server/api/post/[postId]/delete.ts
+++ server/api/post/[postId]/delete.ts
@@ -4,16 +4,29 @@ import { Post } from "types"
 export default defineEventHandler(async (event) => {
     const { postId } = event.context.params!;
     const auth = await authenticateRequest(event);
+    
     let post = await queryOne<Post>([`
         SELECT *
         FROM post:${postId}
+        FETCH images
     `])
+    
     if (post.user === auth.id) {
         var sql = new Array<string>()
-        post.topics.forEach((t: string) => {
-            sql.push(`UPDATE ${t} SET posts -= ${post.id};`)
+        
+        post.topics.forEach(topic => {
+            sql.push(`UPDATE ${topic} SET posts -= ${post.id};`)
         })
+        
+        post.images?.forEach(image => {
+            if (image.user == auth.id) {
+                sql.push(`UPDATE ${auth.id} SET tokens += ${image.tokens};`)
+                sql.push(`DELETE ${image.id};`)
+            }
+        })
+
         sql.push(`DELETE ${post.id}`)
+        
         return await multiQuery(sql)
     }
 })
