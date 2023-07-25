8e6dbb6b1c653c2f7558e33164c08d80e91c557f
tcm <tylermckay10@gmail.com>
2023-07-25 00:24:54 -0300

    setup forgot password resetting

--- components/system/Login.vue
+++ components/system/Login.vue
@@ -1,37 +1,58 @@
 <script setup lang="ts">
 defineProps<{ visible: boolean }>()
 
-const session = getSession();
-const events = useEvents();
+const session = getSession()
+const events = useEvents()
 
-const username = ref("");
-const password = ref("");
+const username = ref("")
+const password = ref("")
 
-const hints = useHints();
+const hints = useHints()
 
-const login = async () => {
+const wrongAttempts = ref(0)
+
+async function attemptLogin() {
     if (await session.login(username.value, password.value)) {
-        events.publish(Trigger.toggleLogin);
+        events.publish(Trigger.toggleLogin)
+        wrongAttempts.value = 0
         username.value = ""
         password.value = ""
     }
     else {
         hints.addError("Username or password were incorrect.")
+        wrongAttempts.value += 1
     }
 }
 
+function forgetPassword() {
+    hints.addWarning('We are still working on this...')
+    // navigateTo("/register")
+    // closeLogin()
+}
+
+function closeLogin() {
+    events.publish(Trigger.toggleLogin)
+    wrongAttempts.value = 0
+}
 </script>
 
 <template>
-    <Popup :visible="visible" title="Login" accept-label="Login" @accept="login" decline-label="Cancel" @decline="events.publish(Trigger.toggleLogin)">
+    <Popup :visible="visible" title="Login" accept-label="Login" @accept="attemptLogin" decline-label="Cancel" @decline="closeLogin">
         <div class="login form">
             <div class="field">
                 <label>Username</label>
                 <input v-model="username" type="text" />
             </div>
             <div class="field">
-                <label>Password</label>
-                <input v-model="password" @keyup.enter="login" type="password" />
+                <label class="forgot" v-if="wrongAttempts >= 3">
+                    <NuxtLink @click="forgetPassword">
+                        Forget your password?
+                    </NuxtLink>
+                </label>
+                <label v-else>
+                    <span>Password</span>
+                </label>
+                <input v-model="password" @keyup.enter="attemptLogin" type="password" />
             </div>
         </div>
     </Popup>
@@ -40,5 +61,13 @@ const login = async () => {
 <style scoped lang="scss">
 .form {
     width: 256px;
+}
+
+label.forgot {
+    a {
+        color: $dox-red;
+        text-decoration: underline;
+        cursor: pointer;
+    }
 }
 </style>
