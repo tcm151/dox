013622646c7a5990c42862fbe1247709c09c55e1
tcm <tylermckay10@gmail.com>
2023-09-01 16:20:43 -0300

    scaffold threads

---
+++ pages/feed/threads.vue
@@ -0,0 +1,17 @@
+<template>
+    <article class="column m-4 p-4">
+        <!-- TODO scaffold topics feed -->
+        <button class="danger">
+            <i class="fa-solid fa-person-digging"></i>
+            This page is still under construction...
+        </button>
+    </article>
+</template>
+
+<style scoped lang="scss">
+article {
+    @include fit-width(400px, 1rem);
+    border-radius: 0.25rem;
+    background-color: $dox-white-ultra;
+}
+</style>
