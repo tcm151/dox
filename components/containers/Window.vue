f8a9142be11993a00656837535cdc58ae4d53be8
tcm <tylermckay10@gmail.com>
2023-11-20 16:04:47 -0400

    updated window styling

--- components/containers/Window.vue
+++ components/containers/Window.vue
@@ -29,20 +29,20 @@ function resizePopup() {
 <template>
     <main class="popup column center" v-if="props.visible">
         <article class="window" ref="window" :style="{ width: width ?? 'fit-content', height: height ?? 'auto', maxWidth: maxWidth, maxHeight: maxHeight }">
-            <section class="title-bar">
-                <div class="left row center-inline g-2 px-4 py-2">
+            <header class="title-bar">
+                <div class="left row center-inline g-2 px-3 py-2">
                     <i :class="icon" v-if="icon"></i>
                     <h2 class="title" v-if="title">{{ title }}</h2>
                 </div>
                 <button class="close px-3" @click="emit('close')">
                     <i class="fa-solid fa-xmark"></i>
                 </button>
-            </section>
+            </header>
             <section class="window-slot">
                 <slot />
             </section>
         </article>
-        <div class="filler" />
+        <!-- <div class="filler" /> -->
     </main>
 </template>
 
@@ -73,20 +73,20 @@ article.window {
     // position: absolute;
 
     @include flex-v;
-    border-radius: 0.25rem;
+    border-radius: 0.5rem;
     background-color: white;
-    box-shadow: 0.25rem 0.5rem 1rem -0.25rem #BBBA, 0 0 0.33rem 1px #CCCA;
+    // box-shadow: 0.25rem 0.5rem 1rem -0.25rem $dox-black-4, 0 0 0.33rem 1px $dox-black-4;
     animation: fade-in 256ms;
 }
 
-section.title-bar {
+header.title-bar {
     @include flex-h (1rem);
     align-items: stretch;
     justify-content: space-between;
     color: $dox-white-0;
     background-color: $dox-black-0;
-    border-top-left-radius: 0.25rem;
-    border-top-right-radius: 0.25rem;
+    border-top-left-radius: 0.5rem;
+    border-top-right-radius: 0.5rem;
     user-select: none;
 
     div.left {
@@ -109,7 +109,7 @@ section.title-bar {
     
     button.close:hover {
         border-radius: 0;
-        border-top-right-radius: 0.25rem;
+        border-top-right-radius: 0.5rem;
         background-color: $dox-red;
     }
 }
@@ -117,7 +117,8 @@ section.title-bar {
 section.window-slot {
     padding: 1rem;
     @include flex-v;
-    border-radius: 0 0 0.25rem 0.25rem;
+    border-radius: 0 0 0.5rem 0.5rem;
+    border: 1px solid $dox-white-2;
     background-color: $dox-white-0;
     overflow-y: auto;
 }
