e0be41eef5be1aff8174d36889421897d9517184
TCM <tylermckay10@gmail.com>
2023-12-04 01:08:54 -0400

    fix collapse children in Tree component

--- components/lists/Tree.vue
+++ components/lists/Tree.vue
@@ -5,64 +5,54 @@ const props = defineProps<{
     getChildren: Function
 }>()
 
-const showChildren = ref(true)
-function toggleChildren() {
-    showChildren.value = !showChildren.value
+const hideChildren = ref<string>("")
+function toggleChildren(id: string) {
+    hideChildren.value = (hideChildren.value == id) ? "" : id
 }
 </script>
 
 <template>
-    <div class="tree" v-if="children.length > 0">
-        <TransitionGroup name="items">
-            <div class="outside" v-for="item in children" :key="item.id">
-                <!-- BUG this collapses all comments -->
-                <div class="comment-line mb-1" @click="toggleChildren"></div>
-                <div class="item">
-                    <slot name="item" :item="item">
-                    </slot>
-                    <Transition name="children">
-                        <div class="ml-3" v-if="showChildren && getChildren(item, items).length > 0">
-                            <Tree :items="items" :children="getChildren(item, items)" :get-children="getChildren">
-                                <template v-for="(_, slot) in $slots" v-slot:[slot]="scope: any">
-                                    <slot :name="slot" v-bind="scope ?? {}">
-                                    </slot>
-                                </template>
-                            </Tree>
-                        </div>
-                    </Transition>
-                </div>
+    <main class="tree" v-if="children.length > 0">
+        <div class="outside row" v-for="item in children" :key="item.id">
+            <aside class="indent-line mb-1 mr-2" :class="{ 'collapsed': hideChildren == item.id }" @click="toggleChildren(item.id)" />
+            <div class="item fill">
+                <slot name="item" :item="item" />
+                <Tree class="ml-3" v-if="hideChildren != item.id" :items="items" :children="getChildren(item, items)" :get-children="getChildren">
+                    <template v-for="(_, slot) in $slots" v-slot:[slot]="scope: any">
+                        <slot :name="slot" v-bind="scope ?? {}">
+                        </slot>
+                    </template>
+                </Tree>
             </div>
-        </TransitionGroup>
-    </div>
+        </div>
+    </main>
 </template>
 
 <style scoped lang="scss">
-.comment-line {
-    width: 3px;
-    margin-right: 0.25rem;
-    border-radius: 0.25em;
-    background-color: $dox-white-2;
+.indent-line {
+    width: 0.33rem;
+    border-radius: 0.25rem;
+    background-color: $dox-white-1;
     cursor: pointer;
 }
 
-.comment-line:hover {
-    background-color: $dox-white-1;
+.indent-line:hover {
+    background-color: $dox-white-2;
 }
 
-.outside {
-    @include flex-h;
+.indent-line.collapsed {
+    background-color: $dox-white-2;
 }
 
 .items-move, .items-enter-active, .items-leave-active {
-    transition: all 256ms ease-in-out;
+    transition: all 512ms ease-in-out;
 }
 
 .items-enter-from, .items-leave-to {
     opacity: 0;
 }
 
-.item {
-    flex: 1 1;
-    @include flex-v;
+.items-leave-active {
+    position: absolute;
 }
 </style>
