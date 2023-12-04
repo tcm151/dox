7f499d9096bc978ded999bb50a911b6af3464bad
tcm <tylermckay10@gmail.com>
2023-12-04 17:04:51 -0400

    fixed cache not working with booleans fixed cache not accounting for multiple users fixed cache not working correctly when not logging in right away

--- services/cache.ts
+++ services/cache.ts
@@ -6,21 +6,26 @@ interface Cache {
 }
 
 export const useCache = defineStore("cache", () => {
+    const events = useEvents()
     const session = getSession()
-    let cache = useLocalStorage<Cache>(`cache:${session.user.id}`, {})
+
+    let cache = ref<Cache>({})
+    events.subscribe(Trigger.authenticatedUser, () => {
+        cache = useLocalStorage<Cache>(`cache:${session.user.id}`, {})
+    })
 
     function get<T>(key: string, fallback?: () => T): Ref<T> {
         // WARN potential for performance degradations here
-        if (cache.value[key]) {
+        if (cache.value[key] != undefined) {
             const cachedRef = ref<T>(cache.value[key]) as Ref<T>
             watch (cachedRef, () => cache.value[key] = cachedRef.value)
             return cachedRef
         }
         else if (fallback) {
-            const fallbackRef = ref<T>(fallback()) as Ref<T>
+            cache.value[key] = fallback()
+            const fallbackRef = ref<T>(cache.value[key]) as Ref<T>
             watch (fallbackRef, () => cache.value[key] = fallbackRef.value)
             return fallbackRef
-
         }
         else {
             throw createError({
