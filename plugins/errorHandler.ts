0ee20b1be287b68ffd5987c6cfe0c997fd413a62
tcm <tylermckay10@gmail.com>
2024-03-20 16:58:56 -0300

    stop logging errors to database

--- plugins/errorHandler.ts
+++ plugins/errorHandler.ts
@@ -1,8 +1,8 @@
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.config.errorHandler = async (error, context) => {
-        await $fetch("/api/error/add", {
-            method: "POST",
-            body: { error, context: null }
-        })
+        // await $fetch("/api/error/add", {
+        //     method: "POST",
+        //     body: { error, context: null }
+        // })
     }
 })
