acecf0ac59f67bffcca5a19e7f305d2c949903b1
tcm <tylermckay10@gmail.com>
2024-03-20 17:10:25 -0300

    fallback for ads when not working...

--- components/ads/InlineAd.vue
+++ components/ads/InlineAd.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-    <aside class="box">
+    <aside class="box grid center">
         <ins
             class="adsbygoogle"
             data-ad-format="fluid"
@@ -10,13 +10,16 @@
             data-ad-client="ca-pub-3080690900599338"
             data-ad-slot="5356268808"
         />
+        <h3>This is an Ad.</h3>
     </aside>
 </template>
 
 <style scoped lang="scss">
 aside.box {
     height: 66px;
-    
+    border: 2px solid $white-2;
+    color: $white-3;
+
     ins.adsbygoogle {
         display: inline-block;
     }
