c92e2c0d2cb34320f12916f5464528eacc627800
tcm <tylermckay10@gmail.com>
2024-06-16 17:00:33 -0300

    log schema result to console

--- server/api/developer/database/sync.ts
+++ server/api/developer/database/sync.ts
@@ -11,5 +11,7 @@ export default defineEventHandler(async (event) => {
 
     let schema = await useStorage("assets:server").getItem<string>("schema.surql") ?? ""
     // let migrations = await useStorage("assets:server").getItem<string>("migrations.surql") ?? ""
+    console.log(schema)
+    
     return await complexQuery({ sql: [schema] })
 })
