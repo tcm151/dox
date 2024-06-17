5ca036c8a2c40dfa9769729941a43f28ea6518cf
tcm <tylermckay10@gmail.com>
2024-06-16 21:34:22 -0300

    remove log user on auth

--- server/utils/auth.ts
+++ server/utils/auth.ts
@@ -12,8 +12,6 @@ export const authenticateRequest = async (event: H3Event) => {
         })
         
         let user = await auth.info() as unknown as User
-        console.log(user)
-    
         await auth.close()
         return user
     }
