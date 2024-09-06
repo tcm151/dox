3e58fe8ffaabcdafbb762611f88e13e135e4a6ee
tcm <tylermckay10@gmail.com>
2024-09-06 12:04:45 -0300

    add draggable directive

--- plugins/directives.ts
+++ plugins/directives.ts
@@ -1,10 +1,32 @@
+import type { DirectiveBinding } from "vue"
+
 export default defineNuxtPlugin((nuxtApp) => {
     
-    nuxtApp.vueApp.directive("focusSelect", {
-        mounted(element: HTMLInputElement, binding, vNode) {
-            element.onfocus = () => {
-                element.select()
-            }
+    nuxtApp.vueApp.directive("focusSelect", (element: HTMLInputElement) => {
+        element.onfocus = () => {
+            element.select()
+        }
+    })
+
+    interface Draggable {
+        dragStart?: (event: DragEvent) => void
+        dragEnter?: (event: DragEvent) => void
+        dragOver?: (event: DragEvent) => void
+    }
+
+    nuxtApp.vueApp.directive("draggable", (element: HTMLElement, binding: DirectiveBinding<Draggable>) => {
+        element.draggable = true
+        element.ondragstart = (event) => {
+            event.preventDefault()
+            binding.value.dragStart?.(event)
+        }
+        element.ondragenter = (event) => {
+            event.preventDefault()
+            binding.value.dragEnter?.(event)
+        }
+        element.ondragover = (event) => {
+            event.preventDefault()
+            binding.value.dragOver?.(event)
         }
     })
 })
