7d7a1242107590bbaebc1f6b26fdb634ad95ff82
TCM <tylermckay10@gmail.com>
2025-03-18 17:57:58 -0300

    simplify caching code

--- services/cache.ts
+++ services/cache.ts
@@ -18,18 +18,13 @@ export const useCache = defineStore("cache", () => {
     events.subscribe(Trigger.userLoggedOut, () => cache = refreshCache())
 
     function get<T>(key: string, fallback: () => T): Ref<T> {
-        // WARN potential for performance degradations here
-        if (cache.value[key] != undefined) {
-            const cachedRef = ref<T>(cache.value[key]) as Ref<T>
-            watch (cachedRef, () => cache.value[key] = cachedRef.value)
-            return cachedRef
-        }
-        else {
+        if (key in cache.value == false) {
             cache.value[key] = fallback()
-            const fallbackRef = ref<T>(cache.value[key]) as Ref<T>
-            watch (fallbackRef, () => cache.value[key] = fallbackRef.value)
-            return fallbackRef
         }
+        
+        const cachedRef = ref<T>(cache.value[key]) as Ref<T>
+        watch (cachedRef, () => cache.value[key] = cachedRef.value)
+        return cachedRef
     }
 
     function set(key: string, value: any) {
