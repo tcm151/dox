a3d5a47bebc88d472c68d62fd4dc2208a9af5bb8
TCM <tylermckay10@gmail.com>
2026-03-04 01:48:03 -0400

    images used in posts are not public

--- server/api/image/index.ts
+++ server/api/image/index.ts
@@ -4,6 +4,7 @@ export default defineEventHandler(async (event) => {
     var { sql } = queryBuilder()
     sql.push('SELECT id, user.id, user.name, votes, type, tokens, time, url')
     sql.push('FROM image')
+    sql.push('WHERE public = true')
     sql.push('ORDER BY time DESC')
     sql.push('FETCH user')
     return await queryAll<Image>({ sql })
