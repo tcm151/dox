6d5d2a63d1a037c15654c094ae3b08140f5e85ea
Tyler McKay <tylermckay@gmail.com>
2026-03-17 12:54:02 -0300

    added a rate limiter to exposed endpoints

---
+++ server/middleware/protection.ts
@@ -0,0 +1,67 @@
+class RateLimiter {
+    #routes: Record<string, { ms: number, max: number }>
+    #buckets: Record<string, number[]> = {}
+    #interval: ReturnType<typeof setInterval>
+
+    constructor(cleanupWindow: number, routes: Record<string, { ms: number, max: number }>) {
+        this.#interval = setInterval(() => this.clearExpired(), cleanupWindow)
+        this.#routes = routes
+    }
+
+    private clearExpired() {
+        for (const ip of Object.keys(this.#buckets)) {
+            if (this.#buckets[ip].length === 0) {
+                delete this.#buckets[ip]
+            }
+        }
+    }
+
+    limitExceeded(ipAddress: string, path: string): boolean {
+        const now = Date.now()
+        const bucket = this.#buckets[ipAddress] ?? []
+        if (this.#routes[path]) {
+            const { ms, max } = this.#routes[path]
+            this.#buckets[ipAddress] = bucket.filter(t => t > now - ms)
+            if (this.#buckets[ipAddress].length >= max) {
+                return true
+            }
+            this.#buckets[ipAddress].push(now)
+        }
+        return false
+    }
+
+    retryAfter(ipAddress: string, path: string): number {
+        const { ms } = this.#routes[path] ?? { ms: 0 }
+        const oldest = this.#buckets[ipAddress][0] ?? Date.now()
+        return Math.ceil((oldest + ms - Date.now()) / 1000)
+    }
+
+    destroy() {
+        clearInterval(this.#interval)
+    }
+}
+
+const MAX_REQUESTS = 5
+const REQUEST_WINDOW = 60_000 // 1 minute
+
+// Clean up every 10 minutes
+const limiter = new RateLimiter(10 * 60 * 1000, {
+    "/api/user/register": { ms: REQUEST_WINDOW, max: MAX_REQUESTS },
+    "/api/profile/login": { ms: REQUEST_WINDOW, max: MAX_REQUESTS },
+    "/api/profile/password/reset": { ms: REQUEST_WINDOW, max: MAX_REQUESTS },
+}) 
+
+export default defineEventHandler((event) => {
+    const path = event.node.req.url.split("?").at(0) ?? ""
+    const ipAddress = getClientIP(event) ?? "unknown"
+
+    if (limiter.limitExceeded(ipAddress, path)) {
+        throw createError({
+            statusCode: 429,
+            statusMessage: "Too Many Requests. Please try again later.",
+            data: {
+                retryAfter: limiter.retryAfter(ipAddress, path)
+            }
+        })
+    }
+})
