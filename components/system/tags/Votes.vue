1dda738c6b3ce8f6130e28097023cff7568ae48f
tcm <tylermckay10@gmail.com>
2026-03-20 17:28:00 -0300

    simplify voting code

--- components/system/tags/Votes.vue
+++ components/system/tags/Votes.vue
@@ -6,21 +6,30 @@ const session = getSession()
 const settings = useSettings()
 
 const props = defineProps<{
-    target: Voteable | null
+    target: Voteable
 }>()
+
+const voteable = computed(() => {
+    return props.target
+})
 
 </script>
 
 <template>
-    <div class="fit row g-1" v-if="target">
+    <div class="fit row g-1">
         <ClientOnly>
-            <span class="tag positive" @click.stop="vote.positive(target)" :class="{ voted: target.votes.positive.includes(session.user.id)}">
-                {{ target.votes.positive.length }}
+            <span class="tag positive" @click.stop="vote.positive(voteable)"
+                :class="{ voted: target.votes.positive.includes(session.user.id)}">
+                {{ voteable.votes.positive.length }}
             </span>
-            <span v-if="settings.app.voting.showMisleading" class="tag misleading" @click.stop="vote.misleading(target)" :class="{ voted: target.votes.misleading.includes(session.user.id)}">
+            <span v-if="settings.app.voting.showMisleading" class="tag misleading"
+                @click.stop="vote.misleading(target)"
+                :class="{ voted: target.votes.misleading.includes(session.user.id)}">
                 {{ target.votes.misleading.length }}
             </span>
-            <span v-if="settings.app.voting.showNegative" class="tag negative" @click.stop="vote.negative(target)" :class="{ voted: target.votes.negative.includes(session.user.id)}">
+            <span v-if="settings.app.voting.showNegative" class="tag negative"
+                @click.stop="vote.negative(target)"
+                :class="{ voted: target.votes.negative.includes(session.user.id)}">
                 {{ target.votes.negative.length }}
             </span>
         </ClientOnly>
--- utils/voting.ts
+++ utils/voting.ts
@@ -1,82 +1,48 @@
 import type { Voteable } from "~/types"
 
+type Vote = "positive" | "misleading" | "negative"
+
 export const useVoting = () => {
 
     const hints = useHints()
     const session = getSession()
+    const types: Vote[] = ["positive", "misleading", "negative"]
 
-    async function positive(item: Voteable | null) {
+    async function updateVote(item: Voteable, type: Vote) {
         if (!session.isAuthenticated) {
             hints.addError("You must be logged in to interact with others.")
             return
         }
 
-        if (!item) {
-            hints.addError("You cannot vote on something that doesn't exist.")
-            return
+        for (let other of types.filter(t => t != type)) {
+            item.votes[other] = item.votes[type].filter(u => u !== session.user.id)
         }
 
-        item.votes.misleading = item.votes.misleading.filter((id) => id !== session.user!.id)
-        item.votes.negative = item.votes.negative.filter((id) => id !== session.user!.id)
-
-        if (!item.votes.positive.includes(session.user!.id)) {
-            item.votes.positive.push(session.user!.id)
-            await session.useApi(`/api/vote/${item.id}/positive`)
+        if (!item.votes[type].includes(session.user.id)) {
+            item.votes[type].push(session.user.id)
+            await session.useApi(`/api/vote/${item.id}/${type}`)
         }
         else {
-            item.votes.positive = item.votes.positive.filter((id) => id !== session.user!.id)
+            item.votes[type] = item.votes[type].filter(u => u !== session.user.id)
             await session.useApi(`/api/vote/${item.id}/reset`)
         }
     }
 
-    async function misleading(item: Voteable | null) {
-        if (!session.isAuthenticated) {
-            hints.addError("You must be logged in to interact with others.")
-            return
-        }
+    async function positive(item: Voteable) {
+        updateVote(item, "positive")
+    }
 
-        if (!item) {
-            hints.addError("You cannot vote on something that doesn't exist.")
-            return
-        }
-
-        item.votes.positive = item.votes.positive.filter((id) => id !== session.user!.id)
-        item.votes.negative = item.votes.negative.filter((id) => id !== session.user!.id)
-        
-        if (!item.votes.misleading.includes(session.user!.id)) {
-            item.votes.misleading.push(session.user!.id)
-            await session.useApi(`/api/vote/${item.id}/misleading`)
-        }
-        else {
-            item.votes.misleading = item.votes.misleading.filter((id) => id !== session.user!.id)
-            await session.useApi(`/api/vote/${item.id}/reset`)
-        }
+    async function misleading(item: Voteable) {
+        updateVote(item, "misleading")
     }
     
-    async function negative(item: Voteable | null) {
-        if (!session.isAuthenticated) {
-            hints.addError("You must be logged in to interact with others.")
-            return
-        }
-
-        if (!item) {
-            hints.addError("You cannot vote on something that doesn't exist.")
-            return
-        }
-        
-        item.votes.positive = item.votes.positive.filter((id) => id !== session.user!.id)
-        item.votes.misleading = item.votes.misleading.filter((id) => id !== session.user!.id)
-        
-        if (!item.votes.negative.includes(session.user!.id)) {
-            item.votes.negative.push(session.user!.id)
-            await session.useApi(`/api/vote/${item.id}/negative`)
-        }
-        else {
-            item.votes.negative = item.votes.negative.filter((id) => id !== session.user!.id)
-            await session.useApi(`/api/vote/${item.id}/reset`)
-        }
-        
+    async function negative(item: Voteable) {
+        updateVote(item, "negative")
     }
 
-    return { positive, misleading, negative }
+    return {
+        positive,
+        misleading,
+        negative
+    }
 }
