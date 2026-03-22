b081a4e50caf8ecf3db3ac8b914b24c7d45c86fa
TCM <tylermckay10@gmail.com>
2026-03-22 01:36:08 -0300

    sorting is hotter now

--- server/utils/sorting.ts
+++ server/utils/sorting.ts
@@ -24,6 +24,13 @@ function sortNew(first: Sortable, second: Sortable) {
     return firstTime < secondTime ? 1 : -1
 }
 
+function sortHot(first: Sortable, second: Sortable) {
+    const now = DateTime.now()
+    const firstAdjustedScore = first.score / Math.max(1, now.diff(DateTime.fromISO(first.time), "days").days)
+    const secondAdjustedScore = second.score / Math.max(1, now.diff(DateTime.fromISO(second.time), "days").days)
+    return firstAdjustedScore < secondAdjustedScore ? 1 : -1
+}
+
 function sortTop(first: Sortable, second: Sortable) {
     if (first.score === second.score) {
         return sortNew(first, second)
@@ -31,13 +38,4 @@ function sortTop(first: Sortable, second: Sortable) {
     else {
         return first.score < second.score ? 1 : -1
     }
-}
-
-function sortHot(first: Sortable, second: Sortable) {
-    const now = DateTime.now()
-    const firstDays = Math.max(1, now.diff(DateTime.fromISO(first.time), "days").days)
-    const secondDays = Math.max(1, now.diff(DateTime.fromISO(second.time), "days").days)
-    const firstAdjustedScore = first.score / (firstDays * (first.visits ?? 1))
-    const secondAdjustedScore = second.score / (secondDays * (second.visits ?? 1))
-    return firstAdjustedScore < secondAdjustedScore ? 1 : -1
 }
