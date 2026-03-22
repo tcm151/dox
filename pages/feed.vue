574405205104ee168c4296457794c00e165f5144
TCM <tylermckay10@gmail.com>
2026-03-22 01:36:28 -0300

    fixed navbar not displaying correctly

--- pages/feed.vue
+++ pages/feed.vue
@@ -19,7 +19,7 @@ const settings = useSettings()
 const tabs = ref<any[]>([
     { route: '/feed/search', icon: 'fa-solid fa-magnifying-glass', label: 'Search', hide: () => !settings.app.feed.showSearch },
     { route: '/feed/topics', icon: 'fa-solid fa-tags', label: 'Topics', hide: () => !settings.app.feed.showTopics },
-    { route: '/feed/discover', icon: 'fa-solid fa-newspaper', label: 'Discover' },
+    { route: '/feed/discover', icon: 'fa-solid fa-house', label: 'Home' },
     { route: '/feed/posts', icon: 'fa-solid fa-newspaper', label: 'Posts' },
     { route: '/feed/threads', icon: 'fa-solid fa-comments', label: 'Threads', hide: () => !settings.app.feed.showThreads },
     { route: '/feed/images', icon: 'fa-solid fa-image', label: 'Images', hide: () => !settings.app.feed.showImages },
@@ -29,7 +29,9 @@ const tabs = ref<any[]>([
 
 <template>
     <article class="feed column center-inline">
-        <PagedTabstrip :tabs="tabs" />
+        <ClientOnly>
+            <PagedTabstrip :tabs="tabs" />
+        </ClientOnly>
         <section class="page column center-inline">
             <NuxtPage :key="route.path" />
         </section>
