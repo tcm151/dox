277bf8bd13e5c77ef9d1e38adc448b21399eb324
TCM <tylermckay10@gmail.com>
2026-03-22 01:36:43 -0300

    post replies are broken still

--- server/api/post/add.ts
+++ server/api/post/add.ts
@@ -4,6 +4,7 @@ export default defineEventHandler(async (event) => {
     const auth = await authenticateRequest(event)
     let post = await readBody<Post>(event)
 
+    // TODO replies need to have their own endpoint
     return await new DatabaseQuery()
         .addSql(`
             CREATE post SET
@@ -18,8 +19,8 @@ export default defineEventHandler(async (event) => {
         .addRecord("user", auth.id)
         .addParameter("title", post.title)
         .addParameter("content", post.content)
-        // .addRecord("replyTo", post.replyTo ?? undefined)
+        // .addRecord("replyTo", post.replyTo)
         .addRecords("topics", post.topics)
-        .addRecords("images", post.images as string[])
+        .addRecords("images", post.images ?? [])
         .queryOne<Post>()
 })
