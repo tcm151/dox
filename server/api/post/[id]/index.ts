095ae8b66cac91f143ec7c99f03f0ff2071b434e
TCM <tylermckay10@gmail.com>
2026-03-23 00:34:59 -0300

    adjust comments on post

--- server/api/post/[id]/index.ts
+++ server/api/post/[id]/index.ts
@@ -1,4 +1,4 @@
-import type { Post, Comment, Sortable } from "~/types"
+import type { Post, Comment } from "~/types"
 
 export default defineEventHandler(async (event) => {
     let query = getQuery<{ sortBy: string }>(event)
@@ -10,11 +10,11 @@ export default defineEventHandler(async (event) => {
             replyTo.id, replyTo.title, topics, votes, archived, edited, timeEdited, visits,
             images,
             (
-                SELECT id, time, user.id, user.name, post.id,
-                replyTo, content, votes, edited, deleted, timeEdited
-                FROM ($post).comments
+                SELECT id, time, user.id, user.name, post,
+                    replyTo, content, votes, score, edited, deleted, timeEdited
+                FROM $post.comments
                 ORDER BY time DESC
-                FETCH user, post
+                FETCH user
             ) AS comments
             FROM $post
             FETCH user, replyTo, images
