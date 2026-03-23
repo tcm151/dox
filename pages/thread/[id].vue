b2f037f33f6d5703bee5f932e66e7b67e0aa6018
tcm <tylermckay10@gmail.com>
2026-03-23 12:41:18 -0300

    allow editing threads

--- pages/thread/[id].vue
+++ pages/thread/[id].vue
@@ -5,13 +5,42 @@ import type { Thread, User } from '~/types'
 
 const route = useRoute()
 const hints = useHints()
-const router = useRouter()
 const events = useEvents()
 const session = getSession()
 
 const id = route.params.id?.toString()
 await useFetch(`/api/thread/${id}/visit`)
 const { data: thread, refresh } = await useFetch<Thread>(`/api/thread/${id}`)
+
+let editingThread = ref(false)
+function toggleEditThread() {
+    editingThread.value = !editingThread.value
+}
+
+// const showPreview = ref(false)
+// function togglePreview() {
+//     showPreview.value = !showPreview.value
+// }
+
+async function updateThread() {
+    if (!thread.value) {
+        hints.addError("You can't edit something that doesn't exist.")
+        return
+    }
+    try {
+        submitting.value = true
+        await session.useApi(`/api/thread/${id}/edit`, { content: thread.value.content })
+        thread.value.edited = true
+        toggleEditThread()
+        await refresh()
+    }
+    catch (error: any) {
+        hints.addError(error.message)
+    }
+    finally {
+        submitting.value = false
+    }
+}
 
 const showReplyBox = ref<boolean>(false)
 const replyText = ref<string>("")
@@ -75,7 +104,7 @@ async function deleteThread() {
 }
 
 async function awardThread() {
-    if ((thread.value?.user as User).id == session.user.id) {
+    if (thread.value?.user.id == session.user.id) {
         hints.addError("You can't award your own threads.")
         return
     }
@@ -125,37 +154,50 @@ function toggleOptions() {
             </button> -->
         </header>
         <section v-if="thread.replyTo" class="mb-2">
-            <ThreadReply :thread="(thread.replyTo as Thread)" />
+            <ThreadReply :thread="thread.replyTo" />
         </section>
         <section class="main box column px-4 pt-4">
             <header class="row-wrap g-1">
                 <Votes :target="thread" />
                 <div class="row-wrap f-1 g-1">
-                    <UserTag class="f-1" :user="(thread.user as User)" />
+                    <UserTag class="f-1" :user="thread.user" />
                     <Tag class="f-1" type="info" icon="fa-chart-simple" :label="thread.visits.toString()" />
-                    <Tag class="f-1" type="info" icon="fa-message" :label="thread.replies.length.toString()" />
-                    <DurationTag class="f-1" :time="thread.time" />
-                    <Tag v-if="thread.timeEdited" class="f-1" type="danger" icon="fa-eraser" :label="formatDate(thread.timeEdited)" />
+                    <Tag class="f-1" type="info" icon="fa-comment" :label="thread.replies.length.toString()" />
+                    <Tag class="f-1" type="info">
+                        <i class="fa-solid fa-stopwatch"></i>
+                        {{ formatDate(thread.time) }}
+                        <template v-if="thread.edited">
+                            <i class="fa-solid fa-eraser"></i>
+                            {{ formatDate(thread.timeEdited) }}
+                        </template>
+                    </Tag>
                 </div>
                 <TopicTag class="f-10" v-for="topic in thread.topics" :topic="topic" />
             </header>
-            <Markdown class="content" :content="thread.content" />
-            <aside v-if="thread.quote" class="quote mb-3 px-3 pt-3">
-                <div class="row-wrap g-1">
-                    <Votes :target="thread.quote" />
-                    <UserTag :user="(thread.quote.user as User)" />
-                    <Tag type="info" icon="fa-chart-simple" :label="thread.quote.visits" />
-                    <Tag type="info" icon="fa-message" :label="thread.quote.replies.length.toString()" />
-                    <DurationTag :time="thread.quote.time" />
-                    <div v-if="thread.quote.topics.length > 0" class="row-wrap g-1">
-                        <TopicTag v-for="topic in thread.quote.topics" :topic="topic" />
+            <template v-if="!editingThread">
+                <Markdown class="content" :content="thread.content" />
+                <aside v-if="thread.quote" class="quote mb-3 px-3 pt-3">
+                    <div class="row-wrap g-1">
+                        <Votes :target="thread.quote" />
+                        <UserTag :user="thread.quote.user" />
+                        <Tag type="info" icon="fa-chart-simple" :label="thread.quote.visits" />
+                        <Tag type="info" icon="fa-comment" :label="thread.quote.replies.length.toString()" />
+                        <DurationTag :time="thread.quote.time" />
+                        <div v-if="thread.quote.topics.length > 0" class="row-wrap g-1">
+                            <TopicTag v-for="topic in thread.quote.topics" :topic="topic" />
+                        </div>
+                        <Tag type="link" icon="fa-right-to-bracket" label="View" @click="navigateTo(`/thread/${extractId(thread.quote.id)}`)" />
                     </div>
-                    <Tag type="link" icon="fa-right-to-bracket" label="View" @click="navigateTo(`/thread/${extractId(thread.quote.id)}`)" />
+                    <Markdown class="content preview" :content="thread.quote.content" />
+                </aside>
+            </template>
+            <ClientOnly>
+                <div v-if="editingThread && thread.user.id === session.user.id" class="field my-4">
+                    <textarea rows="10" v-model="thread.content" />
                 </div>
-                <Markdown class="content preview" :content="thread.quote.content" />
-            </aside>
+            </ClientOnly>
             <footer>
-                <div class="f-1 row-wrap g-1 mb-4" v-if="!showReplyBox && !thread.deleted">
+                <div v-if="!thread.deleted && !editingThread && !showReplyBox" class="f-1 row-wrap g-1 mb-4">
                     <button class="f-1" @click="showReplyBox = true">
                         <i class="fa-solid fa-reply-all fa-flip-horizontal"></i>
                         <span>Reply</span>
@@ -175,7 +217,7 @@ function toggleOptions() {
                         <ExtraOptions 
                             :thread="thread"
                             :visible="showOptions"
-                            @edit=""
+                            @edit="toggleEditThread"
                             @award="awardThread"
                             @report="submitReport(thread.id)"
                             @delete="deleteThread"
@@ -185,11 +227,26 @@ function toggleOptions() {
                         />
                     </ClientOnly>
                 </div>
+                <div v-else-if="editingThread" class="row-wrap f-1 g-1 mb-4">
+                    <ButtonSpinner class="success fill" :loading="submitting" @click="updateThread">
+                        <i class="fa-solid fa-folder-open"></i>
+                        <span>Save</span>
+                    </ButtonSpinner>
+                    <!-- <button class="info fill" @click="togglePreview">
+                        <i class="fa-solid fa-eye" v-if="!showPreview"></i>
+                        <i class="fa-solid fa-eye-slash" v-else></i>
+                        <span>Preview</span>
+                    </button> -->
+                    <button class="danger" @click="toggleEditThread">
+                        <i class="fa-solid fa-ban"></i>
+                        <span>Cancel</span>
+                    </button>
+                </div>
                 <div class="field" v-else-if="showReplyBox">
                     <textarea rows="5" v-model="replyText"></textarea>
                     <div class="row g-2 mt-2">
                         <ButtonSpinner class="success f-1" :loading="submitting" @click="submitThread">
-                            <i class="fa-solid fa-message"></i>
+                            <i class="fa-solid fa-comment"></i>
                             <span>Submit</span>
                         </ButtonSpinner>
                         <button class="danger" @click="showReplyBox = false">
