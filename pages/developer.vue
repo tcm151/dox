1be24cf875e7284753ddb85799b91895d7a0e498
tcm <tylermckay10@gmail.com>
2026-03-26 15:46:44 -0300

    cleanup developer tooling

--- pages/developer.vue
+++ pages/developer.vue
@@ -27,8 +27,7 @@ const route = useRoute()
 const tabs = ref<any[]>([
     { route: '/developer/schema', icon: 'fa-solid fa-table-columns', label: 'Schema' },
     { route: '/developer/query', icon: 'fa-solid fa-terminal', label: 'Database' },
-    { route: '/developer/errors', icon: 'fa-solid fa-triangle-exclamation', label: 'Errors' },
-    { route: '/developer/misc', icon: 'fa-solid fa-ellipsis', label: 'Misc' },
+    { route: '/developer/errors', icon: 'fa-solid fa-circle-exclamation', label: 'Errors' },
 ])
 </script>
 
