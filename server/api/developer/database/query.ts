56fbcbb67c6b2ce2bf370419978df58f8241248f
Tyler McKay <tylermckay@gmail.com>
2026-03-30 14:21:12 -0300

    remove development evironment bypasses

--- server/api/developer/database/query.ts
+++ server/api/developer/database/query.ts
@@ -1,10 +1,10 @@
 export default defineEventHandler(async (event) => {
-    if (!ENV.isDevelopment()) {
-        const auth = await authenticateRequest(event)
-        requireRole(auth, "developer")
-    }
+    const auth = await authenticateRequest(event)
+    requireRole(auth, "developer")
     
     let { query } = await readBody<{ query: string }>(event)
+
+    // TODO add audit logging for queries
     
     return await new DatabaseQuery()
         .addSql(query)
--- server/api/developer/database/sync.ts
+++ server/api/developer/database/sync.ts
@@ -1,8 +1,6 @@
 export default defineEventHandler(async (event) => {
-    if (!ENV.isDevelopment()) {
-        const auth = await authenticateRequest(event)
-        requireRole(auth, "developer")
-    }
+    const auth = await authenticateRequest(event)
+    requireRole(auth, "developer")
 
     let migrations = Buffer.from(await useStorage("assets:server").getItem<string>("migrations.surql") ?? "").toString()
     let schema = Buffer.from(await useStorage("assets:server").getItem<string>("schema.surql") ?? "").toString()
