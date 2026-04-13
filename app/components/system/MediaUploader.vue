fb709fa3a28467b6e77a5564d9b86f3372b027d4
tcm <tylermckay10@gmail.com>
2026-04-12 23:17:25 -0300

    media uploader handles items better

--- app/components/system/MediaUploader.vue
+++ app/components/system/MediaUploader.vue
@@ -5,7 +5,7 @@ interface MediaFile extends File {
 }
 
 const props = defineProps<{
-    media: FileList | null
+    media: FileList
     loading?: boolean
 }>()
 
@@ -13,6 +13,8 @@ const emit = defineEmits<{
     (event: "upload"): void
     (event: "close"): void
 }>()
+
+const selectedItem = ref<File>(props.media[0]!)
 
 function getFileSize(bytes: number) {
     if (bytes >= 1_048_576) {
@@ -30,32 +32,58 @@ function getMediaUrl(image: File) {
 </script>
 
 <template>
-    <Popup title="Upload Media" width="60rem" :loading="loading" :accept="{ label: 'Confirm', action: () => emit('upload') }" :decline="{ label: 'Cancel', action: () => emit('close') }">
-        <div class="media row g-4" v-for="item in media">
-            <section v-if="item.type.startsWith('image/')" class="f-1 b-0">
-                <img :src="getMediaUrl(item)" alt="">
-            </section>
-            <section v-if="item.type.startsWith('audio/')">
-                <audio controls :src="getMediaUrl(item)" />
-            </section>
-            <section class="form f-1 b-0">
-                <div class="field">
-                    <label>File - {{ getFileSize(item.size) }}</label>
-                    <input type="text" v-model="item.name">
-                </div>
-                <div class="field f-1">
-                    <label>Description</label>
-                    <textarea class="f-1" resize="none" v-model="(item as MediaFile).description" />
-                </div>
-            </section>
-        </div>
-    </Popup>
+    <Window title="Media Uploader" width="85%" @close="emit('close')">
+        <template v-for="item in media">
+            <div v-if="selectedItem == item" class="media row g-4">
+                <section v-if="item.type.startsWith('image/')" class="f-1 b-0">
+                    <figure class="image">
+                        <img :src="getMediaUrl(item)" alt="">
+                    </figure>
+                </section>
+                <section v-if="item.type.startsWith('audio/')">
+                    <audio controls :src="getMediaUrl(item)" />
+                </section>
+                <section class="form f-1 b-0">
+                    <div class="field">
+                        <label>File - {{ getFileSize(item.size) }}</label>
+                        <input type="text" v-model="item.name">
+                    </div>
+                    <div class="field f-1">
+                        <label>Description</label>
+                        <textarea class="f-1" resize="none" v-model="(item as MediaFile).description" />
+                    </div>
+                </section>
+            </div>
+            <footer class="row g-2 mt-4">
+                <button class="link">
+                    <i class="fa-solid fa-caret-left"></i>
+                </button>
+                <button class="link f-1">
+                    Upload
+                </button>
+                <button class="link">
+                    <i class="fa-solid fa-caret-right"></i>
+                </button>
+            </footer>
+        </template>
+    </Window>
 </template>
 
 <style scoped lang="scss">
-img {
-    width: stretch;
+div.media {
+    @media (max-aspect-ratio: 1/1) {
+        @include flex-v;
+    }
+}
+
+figure {
     border-radius: 0.25rem;
+    border: 1px solid $white-2;
+    
+    img {
+        max-height: 60vh;
+        object-fit: contain;
+    }
 }
 
 div.field {
